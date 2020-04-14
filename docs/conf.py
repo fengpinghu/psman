4dcf8b478eb6189f07900c2301659c284c42129d
Fengping Hu <fengping_hu@unc.edu>
2020-04-14 10:40:07 -0400

    try with no sphinxarg first

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ if on_rtd:
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
               'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon','sphinxarg.ext']
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
